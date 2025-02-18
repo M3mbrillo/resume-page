21fc7e59262c8f82b1c6eebd871b6b432de08dbe
m3mbrillo <wasinger.facundo@gmail.com>
2025-02-18 16:45:08 -0300

    add page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css?family=Fira+Code" rel="stylesheet">
+</svelte:head>
+
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,212 @@
+<div class="mx-auto flex flex-wrap content-start gap-[2%] p-10">
+	<aside class="w-min rounded-lg bg-gray-100 p-5">
+		<div class="flex flex-col items-center text-center">
+			<div class="h-32 w-32 rounded-full bg-gray-300">
+                <img class="rounded-full" src="https://avatars.githubusercontent.com/u/22511290?s=400&u=b7d812cc968f060e2c086ef2aa37cceca9e37456&v=4"/>
+            </div>
+			<h1 class="mt-4 text-xl font-bold">
+				Facundo Leandro
+				<br />
+				Wasinger
+			</h1>
+			<p class="text-gray-600"><b>Developer_</b></p>
+		</div>
+
+		<div class="mt-6 space-y-4">
+			<div>
+				<h2 class="text-lg font-semibold">Personal Info_</h2>
+				<p class="pl-3 text-gray-700">> Born in: 1994</p>
+				<p class="pl-3 text-gray-700">> Nationality: Argentina</p>
+			</div>
+			<div>
+				<h2 class="text-lg font-semibold">Contact_</h2>
+				<p class="pl-3 text-gray-700">wasinger.facundo@gmail.com</p>
+			</div>
+			<div>
+				<h2 class="text-lg font-semibold">Languages_</h2>
+				<p class="pl-3 text-gray-700">> English</p>
+				<p class="pl-3 text-gray-700">> Spanish</p>
+			</div>
+            <div>
+				<h2 class="text-lg font-semibold">Site_</h2>
+				<p class="pl-3 text-gray-700">> <a href="https://github.com/m3mbrillo" target="_blank">github.com/m3mbrillo</a></p>
+			</div>
+		</div>
+	</aside>
+
+	<main class="w-full p-6 md:w-2/3">
+		<section>
+			<h2 class="text-xl font-semibold">About Me_</h2>
+			<div class="pl-3 text-gray-700">
+				<p>
+					I enjoy solving complex problems and software development. I have experience in business
+					domains such as <b>track and trace of medicine</b>, <b>banking</b>, <b>fintech</b>, and
+					<b>financial services</b>.
+				</p>
+				<p>
+					I am looking to grow in my professional career, and to achieve this goal, I seek a
+					challenging environment that fosters innovation and professional development.
+				</p>
+			</div>
+		</section>
+
+		<section class="mt-6">
+			<h2 class="text-xl font-semibold">Experience_</h2>
+			<ul class="space-y-4">
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Verifarma</span>
+						<span class="font-normal">(2020 - Present)</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							Development of solutions that integrate with specialized hardware in <b
+								>production lines</b
+							>, using both custom proprietary protocols and standard communication methods such as
+							<b>ZPL over USB and TCP/IP</b>.
+							<br />
+							Implementation and maintenance of <b>CI/CD pipelines</b> across multiple technologies
+							(<b>Jenkins</b>, <b>Kubernetes</b>, <b>Helm</b>, <b>Docker</b>, etc.) and programming
+							languages (C#, Rust, PHP, JavaScript).
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Consensus Group</span>
+						<span class="font-normal">(2016 - 2020)</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							Developer backend, frontend, mobile and Tech Leader. Working with <b>fintech</b>,
+							<b>banks</b>, and <b>financial companies</b>.
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> SD-Technology</span>
+						<span class="font-normal">(2013 - 2016)</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>Technical Support L1, L2, L3, IT Analyst and QA.</p>
+					</div>
+				</li>
+			</ul>
+		</section>
+
+		<section class="mt-6">
+			<h2 class="text-xl font-semibold">Technology Skills_</h2>
+			<ul class="space-y-4">
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> C# / .net</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							Dependency Injection (DI), Reflection, <b>LINQ</b>, Multithreading & Parallel
+							Processing,
+							<b>ASP.NET Core</b>, ASP.NET MVC, Spectre Console, <b>Net Core</b> / net 6+, net
+							framework, <b>Entity Framework</b>, Dapper, REST API, SOAP, gRPC, <b>Unit Test</b>
+							(xUnit, NUnit, Moq), <b>Logging</b>
+							(Serilog, Log4net), <b>Mapping</b> (Automapper, Mapperly),
+							<b>Resilience & Retry Strategies</b>
+							(Polly), MediatR, Aspire.
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Database & Message Broker</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							<b>SQL Server</b>, <b>MySQL</b>, MariaDB, <b>PostgreSQL</b>, SQLite, RabbitMQ,
+							ArtemisMQ, Kafka, Redis, ElasticSearch, <b>MongoDB</b>.
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> DevOps / GitOps / SRE</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							<b>Docker</b>, Docker Compose, Podman, <b>Kubernetes</b>, Helm, Jenkins, TeamCity, ArgoCD, HashiCorp
+							Vault, Rancher, RKE2, Digital Ocean k8s Cluster, <b>Amazon EKS</b>, <b>OpenTelemetry</b> (OTEL),
+							Sentry, Azure DevOps, Sonarqube.
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Miscellaneous skills</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							<b>SOLID</b> Principles, <b>KISS</b> Principle, <b>Design Patterns</b>, Architecture Patterns, HTTP
+							Protocol (<b>built a custom HTTP client from scratch</b>), Linux, <b>Git</b>, Visual Source Safe,
+							sFTP, Bucket Storage, Report Tools (Tableau, Crystal Report), Scripting & Automation
+							(python, Bash, PowerShell).
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Security Skills</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+                            <b>Authentication & Authorization</b> (JWT, JWE, SAML, OpenID Connect, OAuth), <b>Security Best
+							Practices</b> (OWASP Top 10, Encryption Algorithms, Public/ Private Key Pairs), <b>Pentesting
+							& Security Tools</b> (Nmap, Burp Suite, Netcat, John The Ripper)
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> ECMAScript / Javascript</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							Bun, <b>Node</b>, npm, yarn, Vue, Angular, <b>Javascript Vanilla</b>, KendoUI, DevExpress, NuxtJS,
+							Nest, JQuery.
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Mobile</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p><b>React Native</b>, Expo, Redux, Flutter</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> PHP</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>
+							<b>Laravel</b>, Symfony, Smarty, Nova, Vapor, PHP New versions and Legacy versions (5.7+)
+						</p>
+					</div>
+				</li>
+				<li>
+					<h3 class="font-semibold">
+						<span class="font-semibold">> Rust</span>
+					</h3>
+					<div class="pl-[2ch] text-gray-700">
+						<p>Tokio, Diesel, Rayon.</p>
+					</div>
+				</li>
+			</ul>
+		</section>
+	</main>
+</div>
+
+<style>
+	* {
+		font-family: 'Fira code';
+	}
+</style>
